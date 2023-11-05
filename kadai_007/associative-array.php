fded16440d3473321195adbd6ea871f7c1b4ffe6
ooyama10091031 <146980622+ooyama10091031@users.noreply.github.com>
2023-11-05 18:46:20 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         $vegetable1_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+          print_r($vegetable1_data);
+         ?>
+     </p>
+ </body>
+ 
+ </html>
